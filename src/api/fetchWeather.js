21aa945dcb264f6a26ad4d252ec15893e78c3470
arash forghanbin <arashforghanbin@gmail.com>
2023-02-26 19:49:03 +0330

    src reset

---
+++ src/api/fetchWeather.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+const URL = "https://api.openweathermap.org/2.5/weather";
+const API_KEY = "f33a48cf794d080148764789aaba32";
+
+export const fetchWeather = async (query) => {
+  const { data } = await axios.get(URL, {
+    params: {
+      q: query,
+      units: "metric",
+      APPID: API_KEY,
+    },
+  });
+  console.log(data);
+  return data;
+};
